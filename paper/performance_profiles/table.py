5c8de4193286b346694aad495a7b0f207e9d865a
StevenElsworth <steven.elsworth@sky.com>
2020-04-22 17:12:05 +0100

    add csv for scl=1

--- paper/performance_profiles/table.py
+++ paper/performance_profiles/table.py
@@ -1,27 +1,23 @@
-################################################################################
-# Generate csv from pickle file.
-
-filename = 'scl0.p'
-
-################################################################################
-
 import os
 import pickle
 import pandas as pd
 
-D = pickle.load(open(filename, 'rb'))
-measures = ['2', 'DTW', '2_diff', 'DTW_diff']
-del D['k'], D['scl'], D['tol']
+for file in ['scl0.p', 'scl1.p']:
+    fname = file[:-2]
 
-# Save to csv
-df = pd.DataFrame.from_dict(D,orient='index').transpose()
-df.to_csv('full_scl0.csv', index=False)
+    D = pickle.load(open(file, 'rb'))
+    measures = ['2', 'DTW', '2_diff', 'DTW_diff']
+    del D['k'], D['scl'], D['tol']
 
-# Remove failed tests
-partial_df = df.loc[df['error'] == 0]
-temp = partial_df['ts_name'].str.split('.tsv_')
-temp = pd.DataFrame(temp.tolist(), index=temp.index)
-temp = temp.drop_duplicates([0])
+    # Save to csv
+    df = pd.DataFrame.from_dict(D,orient='index').transpose()
+    df.to_csv('full_'+fname+'.csv', index=False)
 
-partial_df = partial_df[partial_df.index.isin(temp.index)]
-partial_df.to_csv('partial_scl0.csv', index=False)
+    # Remove failed tests
+    partial_df = df.loc[df['error'] == 0]
+    temp = partial_df['ts_name'].str.split('.tsv_')
+    temp = pd.DataFrame(temp.tolist(), index=temp.index)
+    temp = temp.drop_duplicates([0])
+
+    partial_df = partial_df[partial_df.index.isin(temp.index)]
+    partial_df.to_csv('partial_'+fname+'.csv', index=False)
